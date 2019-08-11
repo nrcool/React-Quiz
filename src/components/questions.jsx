e761c4798eb20e10c69ebe73e16a195728bf6aa1
NAQVI <nomanalam7214@gmail.com>
2019-08-12 00:45:42 +0200

    check answer and increment score and imported result component

--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -1,31 +1,54 @@
 import React from 'react';
 import "./questions.css";
+import Result from "./result";
+
 export default class Questions extends React.Component{
   state={
-    checked:false,
-    index:0
+    checked:[false,false,false,false],
+    index:0,
+    score:0,
+    useranswer:""
   }
-  checkbox=()=>{
+  checkbox=(i,answer)=>{
+    console.log(i)
+    let array=this.state.checked;
+    array[i]=!this.state.checked[i];
     this.setState({
-      checked:!this.state.checked
+      checked:array,
+      useranswer:answer
     })
   }
   nextQuestion=()=>{
-    this.setState({
-      index:this.state.index+1
+    if(this.state.useranswer===this.props.data[this.state.index].correct_answer){
+     this.setState({
+       score:this.state.score+1
+     })
+    }
+       this.setState({
+      index:this.state.index+1,
+      checked:[false,false,false,false]
     })
+   console.log("score is "+this.state.score)
+  }
+  restart=()=>{
+   this.props.restart();
   }
   render(){
-    const newarray=[...this.props.data[this.state.index].incorrect_answers,this.props.data[this.state.index].incorrect_answers]
-    return (
-                    <div className="questionbox">
+    let newarray=[];
+    if(this.state.index<10){
+       newarray=[...this.props.data[this.state.index].incorrect_answers,this.props.data[this.state.index].correct_answer];
+  /* newarray.sort(()=>(Math.round(Math.random())-0.5))  */
+    }
+   
+  return (<div>
+    {this.state.index<10?( <div className="questionbox">
                     <p className="question"><strong>Question:</strong>{this.props.data[this.state.index].question}</p>
                     <div className="mcq">
-                      {newarray.map(option=>{
+                      {newarray.map((option,i)=>{
                         return(
                           <>
-                      <input type="checkbox" id="A" checked={this.state.checked} onChange={this.checkbox}/>
-                        <label className="option" htmlFor="A"><span className="a">A</span><span>{option}</span> 
+                      <input type="checkbox" id={i} value={option} checked={this.state.checked[i]} onChange={()=>this.checkbox(i,option)}/>
+                        <label className="option" htmlFor={i}><span className="a">{i+1}</span><span>{option}</span> 
                       </label>
                       </>
                         )
@@ -34,7 +57,7 @@ export default class Questions extends React.Component{
                     </div>
 
                     <button type="button" onClick={this.nextQuestion} className="btn btn-warning m-2">NEXT</button><br/>
-                    </div>
-                  )
+                    </div> ):(<Result score={this.state.score} restart={this.restart}/>)}
+                    </div>)
 }
 }
