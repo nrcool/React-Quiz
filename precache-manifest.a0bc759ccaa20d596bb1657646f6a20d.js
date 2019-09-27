19c0013c108ab10ac41a117b5ef555c0476075d0
NAQVI <nomanalam7214@gmail.com>
2019-09-27 16:34:13 +0200

    Updates

---
+++ precache-manifest.a0bc759ccaa20d596bb1657646f6a20d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91cfc608242b4ada7f346766b824c7ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e42fc191666247a290d3",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c3a4528781043b2dbc3b",
+    "url": "./static/css/main.5816e03e.chunk.css"
+  },
+  {
+    "revision": "e42fc191666247a290d3",
+    "url": "./static/js/2.baec6a35.chunk.js"
+  },
+  {
+    "revision": "c3a4528781043b2dbc3b",
+    "url": "./static/js/main.7cef49c5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "96a335de2a37ef841245067a91bfee3c",
+    "url": "./static/media/loader.96a335de.gif"
+  },
+  {
+    "revision": "57cec32b3a13878fb030b933452edc18",
+    "url": "./static/media/quest.57cec32b.jpeg"
+  },
+  {
+    "revision": "b5250645827ae40ab0f05248c720db87",
+    "url": "./static/media/quiz.b5250645.png"
+  }
+]);
